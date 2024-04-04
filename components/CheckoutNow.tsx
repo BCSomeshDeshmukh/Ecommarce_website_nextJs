a3981171c28009fdc59ea7630ae721ffafdf52ba
BCSomeshDeshmukh <somesh.deshmukh@blenheimchalcot.com>
2024-04-04 22:13:08 +0530

    button changed to secondary

--- components/CheckoutNow.tsx
+++ components/CheckoutNow.tsx
@@ -24,7 +24,7 @@ export default function CheckoutNow({currency, description,image, name, price,pr
         price_id:price_id
     }
     return(
-        <Button onClick={()=>{
+        <Button variant={"secondary"} onClick={()=>{
             buyNow(product.price_id)
         }}>
             Checkout now
